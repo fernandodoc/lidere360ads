d317f003b7e6942a6f562d9d0c17b4df12941eef
Fernando Doc <81945040+fernandodoc@users.noreply.github.com>
2023-03-21 02:11:44 -0300

    Add files via upload

---
+++ assets/js/nav.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "100vw";
+    document.getElementById("main").style.marginRight = "250px";
+    document.getElementById("mySidebar").style.height = "500px";
+
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+  }
